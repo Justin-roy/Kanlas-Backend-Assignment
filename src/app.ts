5cbc85889bbd3dc64b5862599246816740d57ac2
Sunny Kumar <roysunny951@gmail.com>
2024-05-21 17:35:43 +0530

    added cors

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,8 @@ import express from "express";
 import { connectDB } from "./utils/features.js";
 import { errorMiddleware } from "./middlewares/error.js";
 import { config } from "dotenv";
-import morgan from 'morgan';
+import morgan from "morgan";
+import cors from "cors";
 
 // api's routes imports
 import userRoute from "./routes/user.js";
@@ -11,7 +12,7 @@ import { helloWorld } from "./utils/hello-world.js";
 
 config({
   path: "./.env",
-}); 
+});
 
 const uri = process.env.MONGODB_URI || "";
 const port = process.env.PORT || 4000;
@@ -20,6 +21,16 @@ connectDB(uri);
 const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
+
+// Enable Cross-Origin Resource Sharing (CORS)
+app.use(
+  cors({
+    origin: true,
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
+);
 
 // using route
 app.use(helloWorld);
